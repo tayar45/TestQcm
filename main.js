4f36c1c4f279d6c97faf0e8ca8e4f9640b29dfe6
Tayar <44549344+tayar45@users.noreply.github.com>
2023-09-22 04:06:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+import qcm from "./qcm.js"
+var nb=0
+
+var score=0
+
+var responces=[]
+
+var next =document.getElementById("next")
+
+var question=document.getElementById("question")
+
+var nbQs=document.getElementById("nbQs")
+
+var oprion1=document.querySelector("#option1 span")
+
+var oprion2=document.querySelector("#option2 span")
+
+var oprion3=document.querySelector("#option3 span")
+
+var oprion4=document.querySelector("#option4 span")
+
+var correct_option=qcm[nb].correct_option
+
+nbQs.innerText="السؤال رقم "+(nb+1)
+question.innerText= qcm[nb].question
+oprion1.innerHTML=qcm[nb].options[0]
+oprion2.innerHTML=qcm[nb].options[1]
+oprion3.innerHTML=qcm[nb].options[2]
+oprion4.innerHTML=qcm[nb].options[3]
+
+
+next.onclick=()=> {
+  
+var radioButtons= document.getElementsByName("option")
+var selectedOption = 5;
+
+// Loop through the radio buttons to find the selected one
+for (var i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+        selectedOption = i;
+        break; 
+    }
+    
+}
+responces[nb]=selectedOption
+if (selectedOption==correct_option) {
+  score++
+}
+
+nb++
+
+if (qcm[nb]==undefined) {
+  
+endQcm(score)
+}
+else{
+correct_option=qcm[nb].correct_option
+nbQs.innerText="السؤال رقم "+(nb+1)
+question.innerText= qcm[nb].question
+oprion1.innerHTML=qcm[nb].options[0]
+oprion2.innerHTML=qcm[nb].options[1]
+oprion3.innerHTML=qcm[nb].options[2]
+oprion4.innerHTML=qcm[nb].options[3]
+}
+
+}
+
+
+
+
+function endQcm(score) {
+  
+  var container=document.getElementById("card")
+  var text;
+
+
+
+
+if (score<10) {
+  text="Sir awld l9a7ba t9ra 3la karak"
+}
+else{
+  text="Habibi ma 3lakch"
+}
+
+container.innerHTML=`
+    <div class="card-body text-center">
+    <h5class="card-title"Result</h5>
+    <p class="card-text">Score :${score}</p>
+    <hr>
+     <p class="card-text">${text}</p>
+     
+     <button id="replay">Answers</button>
+  </div>
+`
+
+
+sessionStorage.setItem("responces",JSON.stringify(responces))
+sessionStorage.setItem("qcm",JSON.stringify(qcm))
+
+
+var replay = document.getElementById("replay")
+replay.onclick=function (){
+  window.location.href="/responsesPage.html"
+  
+}
+
+  
+}
+
+
+
+
+
---
+++ responsesPage.js
@@ -0,0 +1,35 @@
+if (sessionStorage.getItem("qcm")==null) {
+  window.location.href="/."
+
+}
+const qcm=JSON.parse(sessionStorage.getItem("qcm"));
+
+const responses=JSON.parse(sessionStorage.getItem("responces"));
+
+
+console.log(responses)
+
+var content = document.getElementById("cont");
+
+for (var i = 0; i < qcm.length; i++) {
+  var div = document.createElement("div");
+  div.className = "card m-2";
+  div.style.width = "auto";
+  div.innerHTML = `
+    <div class="card-body">
+      <h5 class="card-title">Question ${i + 1}</h5>
+      <p class="card-text">${qcm[i].question}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      ${qcm[i].options.map((option, index) => {
+        return `<li class="list-group-item" style='color:white; background:${
+          index == qcm[i].correct_option ? "#33b864" : index == responses[i] ? "#800000" : "#999999"
+        }'>${option}</li>`;
+      }).join('')}
+    </ul>
+  `;
+  content.appendChild(div);
+}
+
+
+sessionStorage.clear()
